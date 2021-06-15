dea8bce6ad5ca0d7a69dbd727315fcf19c5215e1
Hamish Hutchings <hamish.hutchings@grandvision.com>
2021-06-15 10:29:28 +0300

    clean up logging

--- scripts/aks.sh
+++ scripts/aks.sh
@@ -5,9 +5,9 @@ VERSIONS=$(az aks get-versions --location westeurope | jq -r '.orchestrators[] |
 
 for version in ${VERSIONS}; do
 	TAG="aks-${version}"
-	echo "${TAG}"
 	if ! git rev-parse "${TAG}" &>/dev/null; then
 		git tag "${TAG}" || true # If already tagged, don't bother retagging.
+		echo "'${TAG}' tracking"
 	else
 		echo "'${TAG}' already tracked"
 	fi
